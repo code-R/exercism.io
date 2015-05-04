d70768dd431c8bbb5241637bcbdcad98645dcd36
Prasad Surase <prasadsurase@gmail.com>
2015-05-04 13:53:48 +0530

    Fixed the autocomplete issue

--- frontend/app/js/test_output.js
+++ frontend/app/js/test_output.js
@@ -2,18 +2,18 @@ $("a.test_output").click(function() {
   $("p.show-test-output").toggle();
 });
 $("#team_usernames").autocomplete({
-      source: function(request, response){
-           $.ajax({
-               type: "GET",
-               url: "/get_usernames",
-               dataType: "json",               
-               success: function (data) {
-               	   console.log("response", response(data));
-               	   response(data);
-               }              
-           });
-       },
-
+  source: function(request, response){
+    $.ajax({
+      type: "GET",
+      url: "/get_usernames",
+      dataType: "json",               
+      data: {
+        q: request.term
+      },
+      success: function (data) {
+        console.log("response", response(data));
+        response(data);
+      }              
+    });
+  },
 });
-
-
