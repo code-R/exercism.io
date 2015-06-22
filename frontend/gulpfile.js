8bdf6cba28a7e325036edfd52e7d130d8d5f28df
Praveen Puglia <praveenpuglia@gmail.com>
2015-06-22 14:20:12 +0530

    generating default exercism styles via gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    minify = require('gulp-minify-css')
+    minify = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer');
 
 gulp.task('compile:sass', function(){
   gulp.src("stylesheets/application.scss")
     .pipe(sass())
+    .pipe(prefix({
+        browsers : ['last 2 versions'],
+        cascade : false,
+        remove : true
+    }))
     .pipe(minify())
-    .pipe(gulp.dest('generated/css'));
+    .pipe(gulp.dest('bundle/css'));
 }); 
