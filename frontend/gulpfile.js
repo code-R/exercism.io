25e57ff1fee01e53ff82a08dd48f46a8523b3ac4
Praveen Puglia <praveenpuglia@gmail.com>
2015-06-29 13:33:58 +0530

    completed building css using gulp with updated references

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -10,22 +10,30 @@ var paths = {
     fonts : [
         'stylesheets/vendors/font-awesome/fonts/**/*'
     ],
-    scss : ['stylesheets']
+    scss : 'stylesheets/',
+    dist : '../lib/app/public/'
 }
-// Cleaning Directories
+// Cleaning CSS Output Directory
 gulp.task('clean:css', function(){
-    return del(['bundle/css'], function(){
-        console.log("Cleaning CSS Complete");
+    return del([paths.dist+'css'], function(){
+        console.log('Cleaning CSS Complete');
+    });
+});
+
+// Cleaning Fonts Output Directory
+gulp.task('clean:fonts', function(){
+    return del([paths.dist+'fonts'], function(){
+        console.log('Cleaning Fonts Complete');
     });
 });
 
 // Cleaning Everything
-gulp.task('clean:all', ["clean:css"], function(){
+gulp.task('clean:all', ['clean:css', 'clean:fonts'], function(){
 });
 
 // Sass Compilation, prefixing and minification.
 gulp.task('compile:sass',['clean:css'], function(){
-  gulp.src(paths.scss+"/application.scss")
+  gulp.src(paths.scss+"application.scss")
     .pipe(sass())
     .pipe(prefix({
         browsers : ['last 2 versions'],
@@ -33,16 +41,16 @@ gulp.task('compile:sass',['clean:css'], function(){
         remove : true
     }))
     .pipe(minify())
-    .pipe(gulp.dest('bundle/css'));
+    .pipe(gulp.dest(paths.dist+'css'));
 });
 
 // Copy Fonts
-gulp.task("fonts", function(){
+gulp.task('fonts', function(){
     gulp.src(paths.fonts)
-        .pipe(gulp.dest("bundle/fonts"));
+        .pipe(gulp.dest(paths.dist+'fonts'));
 });
 
 // Watch For Changes
-gulp.task("watch", function(){
-  gulp.watch(paths.scss+"/**/*.scss", ["compile:sass"]);
+gulp.task('watch', function(){
+  gulp.watch(paths.scss+'/**/*.scss', ['compile:sass']);
 })
