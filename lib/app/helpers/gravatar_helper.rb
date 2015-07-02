546522c3732b588bbe92aef836ab27b6a8c83672
Praveen Puglia <praveenpuglia@gmail.com>
2015-07-02 16:42:15 +0530

    initial navbar changes

--- lib/app/helpers/gravatar_helper.rb
+++ lib/app/helpers/gravatar_helper.rb
@@ -3,8 +3,9 @@ module Sinatra
 
     def gravatar_tag(url, options = {})
       size = options.fetch(:size) { 20 }
+      theme = options.fetch(:theme)
       url ||= "https://secure.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e"
-      "<img alt=\"\" src=\"#{url}?s=#{size}\" width=\"#{size}\" height=\"#{size}\">"
+      "<img alt=\"\" src=\"#{url}?s=#{size}\" width=\"#{size}\" height=\"#{size}\" class=\"#{theme}\">"
     end
 
   end
