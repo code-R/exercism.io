6ea396b309b890e66df6cec93a1f7fc9af575eed
Praveen Puglia <praveenpuglia@gmail.com>
2015-07-06 18:44:25 +0530

    completed get-started, updated both navs

--- lib/app/helpers/gravatar_helper.rb
+++ lib/app/helpers/gravatar_helper.rb
@@ -3,7 +3,7 @@ module Sinatra
 
     def gravatar_tag(url, options = {})
       size = options.fetch(:size) { 20 }
-      theme = options.fetch(:theme)
+      theme = options.fetch(:theme, 'img-circle')
       url ||= "https://secure.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e"
       "<img alt=\"\" src=\"#{url}?s=#{size}\" width=\"#{size}\" height=\"#{size}\" class=\"#{theme}\">"
     end
