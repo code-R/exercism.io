6f43d9388095a10962d89d2525be771fb521555e
Hanumakanth <hanumakanth@gmail.com>
2015-07-21 10:11:00 +0530

    added javascript analyzer class

--- lib/exercism/code_analyzer.rb
+++ lib/exercism/code_analyzer.rb
@@ -125,3 +125,73 @@ class Java < CodeAnalyzer
     end
   end
 end
+
+class Javascript < CodeAnalyzer
+  def run
+   projectName = user.username + user.id.to_s + git_rep_info.split('/').last
+   xml = "
+   <project>
+      <actions/>
+      <description/>
+      <keepDependencies>false</keepDependencies>
+      <properties>
+        <com.coravy.hudson.plugins.github.GithubProjectProperty plugin='github@1.11.3'>
+          <projectUrl>https://github.com/#{git_rep_info}</projectUrl>
+        </com.coravy.hudson.plugins.github.GithubProjectProperty>
+      </properties>
+      <scm class='hudson.plugins.git.GitSCM' plugin='git@2.3.5'>
+        <configVersion>2</configVersion>
+        <userRemoteConfigs><hudson.plugins.git.UserRemoteConfig>
+          <url>https://github.com/#{git_rep_info}.git</url></hudson.plugins.git.UserRemoteConfig>
+        </userRemoteConfigs>
+        <branches>
+          <hudson.plugins.git.BranchSpec><name>*/master</name></hudson.plugins.git.BranchSpec>
+        </branches>
+        <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+        <submoduleCfg class='list'/><extensions/>
+      </scm>
+      <canRoam>true</canRoam>
+      <disabled>false</disabled>
+      <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+      <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+      <triggers/>
+      <concurrentBuild>false</concurrentBuild>
+      <builders>
+        <hudson.plugins.sonar.SonarRunnerBuilder plugin='sonar@2.2.1'><project/>
+        <properties># required metadata
+          sonar.projectKey=#{projectName}
+          sonar.projectName=#{projectName}
+          sonar.projectVersion=1.0
+          # path to source directories (required)
+          sonar.sources=src
+          sonar.language=js
+          sonar.sourceEncoding=UTF-8
+        </properties>
+        <javaOpts/>
+        <jdk>(Inherit From Job)</jdk>
+        <task/>
+        </hudson.plugins.sonar.SonarRunnerBuilder>
+      </builders>
+      <publishers/><buildWrappers/>
+    </project>"
+    json_params = { :content_type => "application/json" }
+    jobs = RestClient.get "#{jenkins_url}/api/json?tree=jobs[name]", json_params
+    jobs = JSON.parse(jobs)
+    jobs_exist = jobs["jobs"].select{|job| job["name"] == projectName}
+    new_job_url = "#{jenkins_url}/createItem?name=#{projectName}"
+    if jobs_exist.empty?
+      create_job_response = RestClient.post new_job_url, xml, { :content_type => "application/xml" }
+    end
+    build_job_response = nil
+    if jobs_exist.size > 0 || create_job_response.code == 200
+      build_job_response = RestClient.post "#{jenkins_url}/job/"+projectName+"/build", json_params
+    end
+
+    if build_job_response.code == 201
+      return "#{sonarqube_url}/dashboard/index/"+projectName
+    else
+      return "--error--"
+    end
+  end
+end
+
